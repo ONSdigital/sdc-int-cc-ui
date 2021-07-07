e2a6df209bdadb04a221e2657ee190c5edaca87d
Paul-Joel <paul.joel@ons.gov.uk>
2021-07-07 16:08:02 +0100

    Remove GTM from kubernetes dev.yml

--- app/config/dev.py
+++ app/config/dev.py
@@ -4,11 +4,11 @@ import os
 SECRET_KEY = b'secretkey'
 FLASK_DEBUG = 0
 
-if not (CC_SVC_URL := os.getenv('CC_SVC_URL', 'http://localhost')):
-    raise RuntimeError('no CC_SVC_URL ENV variable set')
+if not (CC_SVC_URL := os.getenv('CCSVC_URL', 'http://localhost')):
+    raise RuntimeError('no CCSVC_URL ENV variable set')
 
-if not (CC_SVC_USERNAME := os.getenv('CC_SVC_USERNAME', 'user')):
-    raise RuntimeError('no CC_SVC_USERNAME ENV variable set')
+if not (CC_SVC_USERNAME := os.getenv('CCSVC_USERNAME', 'user')):
+    raise RuntimeError('no CCSVC_USERNAME ENV variable set')
 
-if not (CC_SVC_PWD := os.getenv('CC_SVC_PWD', 'password')):
-    raise RuntimeError('no CC_SVC_PWD ENV variable set')
+if not (CC_SVC_PWD := os.getenv('CCSVC_PASSWORD', 'password')):
+    raise RuntimeError('no CCSVC_PASSWORD ENV variable set')
--- app/config/prod.py
+++ app/config/prod.py
@@ -4,11 +4,11 @@ import os
 if not (SECRET_KEY := os.getenv('SECRET_KEY')):
     raise RuntimeError('no SECRET_KEY ENV variable set')
 
-if not (CC_SVC_URL := os.getenv('CC_SVC_URL')):
-    raise RuntimeError('no CC_SVC_URL ENV variable set')
+if not (CC_SVC_URL := os.getenv('CCSVC_URL')):
+    raise RuntimeError('no CCSVC_URL ENV variable set')
 
-if not (CC_SVC_USERNAME := os.getenv('CC_SVC_USERNAME')):
-    raise RuntimeError('no CC_SVC_USERNAME ENV variable set')
+if not (CC_SVC_USERNAME := os.getenv('CCSVC_USERNAME')):
+    raise RuntimeError('no CCSVC_USERNAME ENV variable set')
 
-if not (CC_SVC_PWD := os.getenv('CC_SVC_PWD')):
-    raise RuntimeError('no CC_SVC_PWD ENV variable set')
+if not (CC_SVC_PWD := os.getenv('CCSVC_PASSWORD')):
+    raise RuntimeError('no CCSVC_PASSWORD ENV variable set')
