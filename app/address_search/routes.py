fc334ffdb013e2426cc523e813dce135267e6bb0
Paul-Joel <paul.joel@ons.gov.uk>
2021-08-09 12:46:24 +0100

    Updates to address input entry point

--- app/address_search/routes.py
+++ app/address_search/routes.py
@@ -1,5 +1,4 @@
 from . import address_search_bp
-from app import app
 from flask import render_template, request, redirect, url_for, flash, current_app, get_flashed_messages
 from app.utils import CCSvc, ProcessPostcode, Common
 from app.errors.handlers import InvalidDataError
@@ -8,18 +7,46 @@ from app.errors.handlers import InvalidDataError
 @address_search_bp.route('/addresses/', methods=['GET', 'POST'])
 async def address_input():
     if request.method == 'POST':
-        return redirect(url_for('address_search.addresses_by_input', input=request.form['form_address_input']))
+        form_input = request.form['form_address_input']
+        if form_input:
+            return redirect(url_for('address_search.addresses_by_input', input=request.form['form_address_input']))
+        else:
+            current_app.logger.info('Address input error: No value entered')
+            flash('Enter an address value', 'error_input')
+            return redirect(url_for('address_search.address_input'))
+
     else:
-        return render_template('address-search/address-input.html')
+        page_title = 'Enter an address'
+        error_input = {}
+        if get_flashed_messages():
+            page_title = Common.page_title_error_prefix + page_title
+            for message in get_flashed_messages():
+                error_input = {'id': 'error_input', 'text': message}
+        return render_template('address-search/address-input.html',
+                               page_title=page_title,
+                               error_input=error_input)
 
 
 @address_search_bp.route('/addresses/input/', methods=['GET', 'POST'])
 async def addresses_by_input():
     if request.method == 'POST':
-        return redirect(url_for('uprn_list', uprn=request.form['form-pick-address']))
+        if 'form-pick-address' in request.form:
+            if request.form['form-pick-address'] == 'xxxx':
+                return redirect(url_for('address_search.address_not_found', address_input=request.args.get('input')))
+            else:
+                return redirect(url_for('uprn_list', uprn=request.form['form-pick-address']))
+        else:
+            flash('Select an address', 'error_selection')
+            return redirect(url_for('address_search.addresses_by_input', input=request.args.get('input')))
     else:
+        page_title = 'Select an address'
+        error_selection = {}
+        if get_flashed_messages():
+            page_title = Common.page_title_error_prefix + page_title
+            for message in get_flashed_messages():
+                error_selection = {'id': 'error_selection', 'text': message}
         if request.args.get('input'):
-            app.logger.info('Starting')
+            current_app.logger.info('Starting')
             cc_return = await CCSvc.get_addresses_by_input(request.args.get('input'))
 
             address_options = []
@@ -48,9 +75,12 @@ async def addresses_by_input():
                 'total_matches': cc_return['total']
             }
 
-            return render_template('address-search/address-results.html', results=address_content)
+            return render_template('address-search/address-results.html',
+                                   page_title=page_title,
+                                   results=address_content,
+                                   error_selection=error_selection)
         else:
-            return render_template('500.html')
+            return render_template('errors/500.html')
 
 
 @address_search_bp.route('/addresses/postcode/', methods=['GET', 'POST'])
@@ -64,7 +94,7 @@ async def postcode_input():
             current_app.logger.info(exc)
             flash(exc.message, 'error_mobile')
             return redirect(url_for('address_search.postcode_input',
-                                   value_postcode=postcode_unvalidated))
+                                    value_postcode=postcode_unvalidated))
     else:
         page_title = 'Enter a postcode'
         error_postcode = {}
@@ -128,7 +158,7 @@ async def addresses_by_postcode(postcode):
                                    results=address_content,
                                    error_selection=error_selection)
         else:
-            return render_template('500.html')
+            return render_template('errors/500.html')
 
 
 @address_search_bp.route('/addresses/address-not-found', methods=['GET'])
@@ -136,4 +166,5 @@ async def address_not_found():
     page_title = 'Address not found'
     return render_template('address-search/address-not-found.html',
                            page_title=page_title,
-                           postcode=request.args.get('postcode'))
+                           postcode=request.args.get('postcode'),
+                           address_input=request.args.get('address_input'))
