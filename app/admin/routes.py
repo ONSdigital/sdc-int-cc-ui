7600ef179092992b1c2be498682d1aae2c8a90ad
Paul-Joel <paul.joel@ons.gov.uk>
2021-10-13 12:02:08 +0100

    Proto Landing Pages Add user list screen in Admin

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -5,3 +5,8 @@ from flask import render_template
 @admin_bp.route('/admin/')
 async def admin_home():
     return render_template('admin/home.html')
+
+
+@admin_bp.route('/admin/user-list/')
+async def admin_user_list():
+    return render_template('admin/user-list.html')
