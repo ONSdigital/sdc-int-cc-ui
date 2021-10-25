25d78a4d1d1548c52d6c394fd65cae627e26722b
Paul-Joel <paul.joel@ons.gov.uk>
2021-10-25 12:16:01 +0100

    Tweaks to config settings

--- app/config/dev.py
+++ app/config/dev.py
@@ -2,8 +2,10 @@
 import os
 import redis
 
-SECRET_KEY = b'secretkey'
 FLASK_DEBUG = 0
+
+if not (SECRET_KEY := os.getenv('SECRET_KEY', 'secretkey')):
+    raise RuntimeError('no SECRET_KEY ENV variable set')
 
 if not (CC_SVC_URL := os.getenv('CCSVC_URL', 'http://localhost:8171/ccsvc')):
     raise RuntimeError('no CCSVC_URL ENV variable set')
@@ -14,13 +16,10 @@ if not (CC_SVC_USERNAME := os.getenv('CCSVC_USERNAME', 'user')):
 if not (CC_SVC_PWD := os.getenv('CCSVC_PASSWORD', 'password')):
     raise RuntimeError('no CCSVC_PASSWORD ENV variable set')
 
-if not (PERMANENT_SESSION_LIFETIME := os.getenv('SESSION_AGE', 2700)):
-    raise RuntimeError('no SESSION_AGE ENV variable set')
-
 if not (REDIS_SERVER := os.getenv('REDIS_SERVER', 'localhost')):
     raise RuntimeError('no REDIS_SERVER ENV variable set')
 
 if not (REDIS_PORT := os.getenv('REDIS_PORT', '6379')):
     raise RuntimeError('no REDIS_PORT ENV variable set')
 
-SESSION_REDIS = redis.Redis(host=REDIS_SERVER, port=REDIS_PORT, ssl_cert_reqs=None)
+SESSION_REDIS = redis.Redis(host=REDIS_SERVER, port=REDIS_PORT, retry_on_timeout=True)
--- app/config/test.py
+++ app/config/test.py
@@ -16,13 +16,10 @@ if not (CC_SVC_USERNAME := os.getenv('CCSVC_USERNAME', 'user')):
 if not (CC_SVC_PWD := os.getenv('CCSVC_PASSWORD', 'password')):
     raise RuntimeError('no CCSVC_PASSWORD ENV variable set')
 
-if not (PERMANENT_SESSION_LIFETIME := os.getenv('SESSION_AGE', '2700')):
-    raise RuntimeError('no SESSION_AGE ENV variable set')
-
 if not (REDIS_SERVER := os.getenv('REDIS_SERVER', 'localhost')):
     raise RuntimeError('no REDIS_SERVER ENV variable set')
 
-if not (REDIS_PORT := os.getenv('REDIS_PORT', '7379')):
+if not (REDIS_PORT := os.getenv('REDIS_PORT', '6379')):
     raise RuntimeError('no REDIS_PORT ENV variable set')
 
-SESSION_REDIS = redis.Redis(host=REDIS_SERVER, port=REDIS_PORT)
+SESSION_REDIS = redis.Redis(host=REDIS_SERVER, port=REDIS_PORT, retry_on_timeout=True)
