8c734922075bf8f0c660312d433d323865adf8c8
Rob Catling <github@rpc-soft.com>
2022-01-24 08:52:31 +0000

    SOCINT-145 settings for testing in DEV

--- app/settings.py
+++ app/settings.py
@@ -59,4 +59,5 @@ SESSION_USE_SIGNER = True
 SESSION_COOKIE_NAME = 'ons_cc'
 PERMANENT_SESSION_LIFETIME = 2700
 SESSION_REDIS = Redis(host=REDIS_SERVER, port=REDIS_PORT, retry_on_timeout=True)
-SAML_PATH = str(Path(__file__).resolve().parent.parent / 'saml')
+SAML_DIR = os.getenv('SAML_DIR', 'saml')
+SAML_PATH = str(Path(__file__).resolve().parent.parent / SAML_DIR)
